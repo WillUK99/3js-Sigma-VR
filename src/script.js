e94a9f084c48b3599f74c9e8dcaabe0a3485cd0d
WillUK99 <willuk1999@gmail.com>
2021-11-26 16:03:32 +0000

    Designs added and scss file struc started

--- src/script.js
+++ src/script.js
@@ -4,8 +4,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { AnaglyphEffect } from 'three/examples/jsm/effects/AnaglyphEffect.js';
 import { GUI } from 'dat.gui'
 import { DoubleSide, PointLightHelper } from 'three'
+import gsap from 'gsap/all';
 
-
+const log = console.log
 
 /**
  * Base
@@ -15,7 +16,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x131313)
+scene.background = new THREE.Color(0x0B0C10)
 
 /**
  * Sizes
@@ -64,10 +65,10 @@ const getMaterial = () => {
     const material = new THREE.MeshPhysicalMaterial({
         color: 0x3f07c5,
         metalness: 1
-        
+
     })
 
-    material.onBeforeCompile = function(shader) {
+    material.onBeforeCompile = function (shader) {
         shader.uniforms.uTime = { value: 0.0 }
 
         shader.vertexShader = `uniform float uTime;\n` + shader.vertexShader
@@ -94,12 +95,12 @@ const getMaterial = () => {
             `
            float diff = dot(vec3(1),vNormal);
 
-           vec3 a = vec3(.2, 0.2, 1.0);
-           vec3 b = vec3(0.5, .5, 0.5);
-    	   vec3 c = vec3(0.12, 1.0, 10.0);
-           vec3 d = vec3(0.30, 2.20, 0.20);
+            vec3 a = vec3(0.5, 0.5, 5.5);
+              vec3 b = vec3(0.5, 1.0, 5.5);
+              vec3 c = vec3(1.0, 1.0, 1.0);
+              vec3 d = vec3(0.0, 0.0, 0.0);
            
-           vec3 cc =  a + b / cos(2.*3.141592*(c*diff+d + uTime/3.0));
+           vec3 cc =  a + b / cos(2.*3.141592*(c*diff+d + uTime/20.0));
            
            diffuseColor.rgb = vec3(diff, 0.0, 0.0);
            diffuseColor.rgb = cc;
@@ -152,11 +153,11 @@ const updateLight = () => {
  */
 // Color gui
 class ColorGUIHelper {
-    constructor(object, prop){
+    constructor(object, prop) {
         this.object = object
         this.prop = prop
         console.log(this.object)
-    }   
+    }
     get value() {
         return `#${this.object[this.prop].getHexString()}`
     }
@@ -181,6 +182,22 @@ const makeXYZGUI = (gui, vec3, name, onChangeFn = undefined) => {
 
 // makeXYZGUI(gui, pointLight.position, "position", updateLight)
 
+/**
+ * Mouse Interactions
+ */
+let targetX
+let targetY
+const mouseKnotXY = {}
+const knotCanvas = document.querySelector(".webgl")
+
+const onKnotCanvasMouseMove = (e) => {
+    mouseKnotXY["x"] = e.clientX - (window.innerWidth / 2)
+    mouseKnotXY["y"] = e.clientY - (window.innerHeight / 2)
+}
+
+knotCanvas.addEventListener("mousemove", onKnotCanvasMouseMove)
+
+
 
 /**
  * Animate
@@ -194,16 +211,29 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - lastElapsedTime
     lastElapsedTime = elapsedTime
-    
-    if(material.userData.shader) {
+
+    if (material.userData.shader) {
         material.userData.shader.uniforms.uTime.value = elapsedTime
+        // material.userData.shader.uniforms.uMouse.value = mouseKnotXY
     }
-    
-    torusOne.rotation.z += deltaTime/2
-    torusOne.rotation.y += deltaTime/4
+
+    targetX = mouseKnotXY.x / 1000
+    targetY = mouseKnotXY.y / 1000
+ 
+    torusOne.rotation.y = elapsedTime / 3
+    torusOne.rotation.z = elapsedTime / 4
+
+    // torusOne.rotation.y += 0.2 * (targetX - torusOne.rotation.y)
+    // torusOne.rotation.z += -0.5 * (targetY - torusOne.rotation.z)
+
+    // gsap.to(torusOne.rotation, {
+    //     duration: 0.2,
+    //     z: `+= -0.5 * (targetY - ${torusOne.rotation.z})`
+    // })
+
     // pointLight.position.x += Math.sin(elapsedTime)/10
     // pointLight.intensity += Math.sin(elapsedTime)/100
-    
+
     // pointLight.position.x = THREE.MathUtils.lerp(-5, 5, Math.abs(Math.sin(elapsedTime)))
 
     // Update controls
@@ -217,3 +247,35 @@ const tick = () => {
 }
 
 tick()
+
+//testing arrows to follow cursor
+// .cursor-arrow
+// const offset = 40
+// const arrowsArr = document.querySelector(".arrow-cursor")
+// const arrowsBoundingRectsArr = arrowsArr.map((arrow) => arrow.getClientRects()[0])
+
+// window.addEventListener("mousemove", (e) => {
+//     for (let arrow of arrowsBoundingRectsArr) {
+//         const centerOfArrow = [
+//             arrow.x + arrow.width / 2,
+//             arrow.y + arrow.height / 2
+//         ]
+
+//         const angle = Math.atan2(e.pageX - centerOfArrow[0], - (e.pageY - centerOfArrow[1]) * (180 / Math.PI))
+//         log(angle)
+//         arrow.style.setProperty('--angle', `${angle - offset}deg`)
+//     }
+// })
+
+// const boxCenter = [
+//     rects.x + rects.width / 2,
+//     rects.y + rects.height / 2
+// ]
+
+// window.addEventListener('mousemove', (event) => {
+//     const angle = Math.atan2(
+//         event.pageX - boxCenter[0], - (event.pageY - boxCenter[1])
+//     ) * (180 / Math.PI)
+
+//     arrowsArr.style.setProperty('--angle', `${angle - offset}deg`)
+// })
